72ffc33a1c1b70b2e63875d03f2a12fd911a4a58
xTCry [Vladislav Kh] <wolt6@mail.ru>
2024-03-10 19:42:10 +0300

    feat: add changing theme mode button
    auto detect and watch theme mode
    update header mobile menu

--- src/frontend/js/modules/extensions.ts
+++ src/frontend/js/modules/extensions.ts
@@ -1,5 +1,12 @@
 import Misprints from '@codexteam/misprints';
 
+import { Storage } from '../utils/storage';
+
+/**
+ * Local storage key
+ */
+const THEME_STORAGE_KEY = 'theme_mode';
+
 /**
  * @class Extensions
  * @classdesc Class for extensions module
@@ -9,8 +16,56 @@ export default class Extensions {
     chatId: window.config.misprintsChatId,
   });
 
+  themeMode: string = 'light';
+
   /**
    * Initialize extensions
    */
-  constructor() {}
+  constructor() {
+    this.initTheme();
+  }
+
+  initTheme() {
+    const updateButtons = (isDark: boolean) => {
+      const buttons = document.querySelectorAll<HTMLElement>(
+        '[name="theme-toggle"]',
+      );
+      for (const button of buttons) {
+        const label = `Change to ${isDark ? 'dark' : 'light'} theme`;
+        button.setAttribute('title', label);
+        button.setAttribute('aria-label', label);
+        // button.innerText = label;
+      }
+    };
+
+    const updateTheme = (theme: string) => {
+      updateButtons(theme === 'dark');
+      document.querySelector('html')!.setAttribute('data-theme', theme);
+    };
+
+    const changeTheme = (theme?: string) => {
+      this.themeMode = theme || (this.themeMode === 'dark' ? 'light' : 'dark');
+      themeModeStorage.set(this.themeMode);
+      updateTheme(this.themeMode);
+    };
+
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const themeModeStorage = new Storage(THEME_STORAGE_KEY);
+    this.themeMode =
+      themeModeStorage.get() || (mediaQuery.matches ? 'dark' : 'light');
+
+    mediaQuery.addEventListener('change', (event) => {
+      changeTheme(event.matches ? 'dark' : 'light');
+    });
+
+    const headerWrapper = document.querySelector('.docs-header')!;
+    headerWrapper.addEventListener('click', (event) => {
+      const target = event.target as HTMLElement;
+      if (target.closest('[name="theme-toggle"]')) {
+        changeTheme();
+      }
+    });
+
+    updateTheme(this.themeMode);
+  }
 }
